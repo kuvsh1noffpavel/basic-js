fd06027e25c9d13f22f9391ad6802f73adb643d1
Pavel Kuvshinov <kuvsh1noffpavel@mail.ru>
2021-09-14 20:51:20 +0300

    feat: finished task what-season
    Finished task what-season.

--- src/what-season.js
+++ src/what-season.js
@@ -11,7 +11,30 @@ import { NotImplementedError } from '../extensions/index.js';
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-export default function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getSeason(date) {
+
+  if (!date){
+    return "Unable to determine the time of year!";
+  }
+
+  if (Object.prototype.toString.call(date) !== '[object Date]'){
+    throw new Error ('Invalid date!');
+  }
+  const monthCount = date.getMonth();
+
+  if (date instanceof Date){
+
+    if (monthCount === 0 || monthCount === 1 || monthCount === 11){
+      return "winter";
+    } else if (monthCount === 2 || monthCount === 3 || monthCount === 4){
+      return "spring";
+    } else if (monthCount === 5 || monthCount === 6 || monthCount === 7){
+      return "summer";
+    } else if (monthCount === 8 || monthCount === 9 || monthCount === 10){
+      return "autumn";
+    } 
+  } else {
+    throw new Error ('Invalid date!');
+  }
+
 }
