5fd3f0f2f4541eb0d3bfca42210acdb91450f9f7
Pavel Kuvshinov <kuvsh1noffpavel@mail.ru>
2021-09-14 20:33:06 +0300

    feat: finished task carbon-dating
    Finished task carbon-dating. All test pasted.

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,7 +17,22 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-export default function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function dateSample(value) {
+  
+  let regExp = /^[0-9.]{0,300}$/u ; // Регулярное выражение на проверку цифр и точки
+  let res =  regExp.test(value);
+
+  if(typeof(value) !== 'string' || value.length == 0 || res == false){
+    return false;
+  } 
+ 
+  let sampleActivity = parseFloat(value);  
+
+  if (sampleActivity > 15 || sampleActivity <= 0){
+    return false;
+  }
+
+  let result = Math.ceil((Math.log(MODERN_ACTIVITY/sampleActivity))/(Math.LN2/HALF_LIFE_PERIOD));
+
+  return result;
 }
