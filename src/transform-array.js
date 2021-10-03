77528154837502f0f24c75811f0a3f47742a9f37
Pavel Kuvshinov <kuvsh1noffpavel@mail.ru>
2021-10-03 23:15:33 +0300

    feat: end task "transform-array"
    End task "transform-array" with one failing test.

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,7 +13,44 @@ import { NotImplementedError } from '../extensions/index.js';
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function transform(arr) {
+  if (!Array.isArray(arr)) throw new Error ("'arr' parameter must be an instance of the Array!");
+
+  let resultArr = [];
+
+  for (let i = 0; i < arr.length; i++){
+    if (arr[i] == '--double-next'){
+      if (i == arr.length-1){
+        continue;
+      } else {
+        resultArr.push(arr[i+1]);
+        continue;
+      }
+    } else if (arr[i] == '--double-prev'){	
+      if (i == 0){
+        continue;
+      } else {
+        resultArr.splice(i, 0, arr[i-1]);
+        continue;
+      }
+    } else if (arr[i] == '--discard-next'){
+      if (i == arr.length-1){
+        continue;
+      } else {
+        i++;
+        continue;
+      }
+    } else if (arr[i] == '--discard-prev'){
+      if (i==0){
+        continue;
+      } else {
+        resultArr.splice(i-1, 1);
+        continue;
+      }
+      
+    }
+    resultArr.push(arr[i]);
+  }
+
+  return resultArr;
 }
